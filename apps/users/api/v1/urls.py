c946633bda2985cacee06e4c26aca80584a6ef79
Oliver Urones <Crower_17@hotmail.com>
2022-11-01 17:38:15 +0100

    Implemented api authorization endpoint

--- apps/users/api/v1/urls.py
+++ apps/users/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import ProfileViewSet
 
@@ -10,4 +11,5 @@ router_users.register(r'profile', ProfileViewSet, basename='profile')
 
 urlpatterns = [
     path(r'', include(router_users.get_urls())),
+    path(r'api-auth', obtain_auth_token)
 ]
