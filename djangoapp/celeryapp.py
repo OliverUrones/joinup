1a6b886ebad0ff7dea40c5b17f5d91beb5761b2b
Oliver Urones <Crower_17@hotmail.com>
2022-11-02 14:45:38 +0100

    Implemented config celery app

---
+++ djangoapp/celeryapp.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+import os
+
+from celery import Celery
+
+from djangoapp.settings import env, ENVIRONMENT
+
+
+"""
+Celery app configuration
+celery -A djangoapp.celeryapp worker -Q email,sms -E --loglevel=INFO
+"""
+
+celery_backend = env('CELERY_BACKEND', default='redis://localhost:6379/0')
+celery_broker = env('CELERY_BROKER', default='amqp://guest:guest@localhost:5672//')
+
+# To register task into worker
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoapp.settings')
+
+app = Celery('djangoapp.celeryapp', backend=celery_backend, broker=celery_broker)
+
+
+# Uncomment to execute task synchronous into local environment
+# if ENVIRONMENT == 'local':
+#     app.conf.update(
+#         CELERY_ALWAYS_EAGER=True,
+#     )
