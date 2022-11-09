6bb9b35329f362aec1d0b542bf6100e9e6fee797
Oliver Urones <Crower_17@hotmail.com>
2022-11-09 13:16:42 +0100

    Fixed 500 errors. Fix security bug when get profile. Change queryset method to check list action and not anonymous user. Not allow detail method to recover profile. Allow list method to recover profile.

--- apps/users/api/v1/views.py
+++ apps/users/api/v1/views.py
@@ -1,10 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.status import HTTP_200_OK, HTTP_405_METHOD_NOT_ALLOWED
+from rest_framework.status import HTTP_200_OK, HTTP_405_METHOD_NOT_ALLOWED, HTTP_401_UNAUTHORIZED
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import ProfileSignupSerializer, ProfileDetailSerializer
-from apps.users.models import Profile
 
 
 class ProfileViewSet(ModelViewSet):
@@ -19,25 +18,27 @@ class ProfileViewSet(ModelViewSet):
     serializer_class = ProfileSignupSerializer
     serializer_action_classes = {
         'create': ProfileSignupSerializer,
-        'retrieve': ProfileDetailSerializer
+        'list': ProfileDetailSerializer
     }
 
     def get_queryset(self):
-        print("get_queryset method")
-        if self.action == 'retrieve' and self.request.user:
-            return Profile.objects.get(pk=self.kwargs.get('pk'))
+        if self.action == 'list' and not self.request.user.is_anonymous:
+            return self.request.user
 
     def get_serializer_class(self):
         return self.serializer_action_classes[self.action]
 
     def retrieve(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer_class()
-        user = serializer(queryset)
-        return Response(user.data, status=HTTP_200_OK)
+        return Response({'message': 'Method not allowed'}, HTTP_405_METHOD_NOT_ALLOWED)
 
     def list(self, request, *args, **kwargs):
-        return Response({'message': 'Method not allowed'}, HTTP_405_METHOD_NOT_ALLOWED)
+        queryset = self.get_queryset()
+        if queryset:
+            serializer = self.get_serializer_class()
+            user = serializer(queryset)
+            return Response(user.data, status=HTTP_200_OK)
+        else:
+            return Response({'message': 'Not Authorized'}, HTTP_401_UNAUTHORIZED)
 
     def update(self, request, *args, **kwargs):
         return Response({'message': 'Method not allowed'}, HTTP_405_METHOD_NOT_ALLOWED)
